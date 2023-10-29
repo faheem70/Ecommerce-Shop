6c8f9e81d5a3bbc975570d9ff22dbfbf62ed561a
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-10-29 11:13:06 +0530

    payment

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -2,7 +2,7 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const Product = require('../models/productModel');
 const Purchase = require('../models/purchaseModel'); // Import your Purchase model
-
+const Order = require("../models/orderModel");
 exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 
   const { amount, productId } = req.body;
@@ -31,4 +31,31 @@ exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 
 exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+});
+
+exports.processPaymentCod = catchAsyncErrors(async (req, res) => {
+  try {
+    const { amount, productId, shippingInfo, orderItems, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+    // Create a new order
+    const newOrder = new Order({
+      shippingInfo,
+      orderItems,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    // Save the order to the database
+    await newOrder.save();
+
+    // You can also handle other order-related operations here (e.g., sending confirmation emails, etc.).
+
+    // Respond with a success message
+    res.status(200).json({ success: true, message: 'Order placed successfully.' });
+  } catch (error) {
+    // Handle errors and send an error response
+    res.status(500).json({ success: false, error: 'Failed to place Cash on Delivery order.' });
+  }
 });
--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   processPayment,
   sendStripeApiKey,
+  processPaymentCod,
 } = require("../controllers/paymentController");
 const router = express.Router();
 const { isAuthenticatedUser } = require("../middleware/auth");
@@ -9,5 +10,5 @@ const { isAuthenticatedUser } = require("../middleware/auth");
 router.route("/payment/process").post(isAuthenticatedUser, processPayment);
 
 router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
-
+router.route("/payment/cod", processPaymentCod);
 module.exports = router;
