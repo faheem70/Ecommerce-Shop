2065bac80d101f69cc9931194f60e08227336afa
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-11-14 12:11:08 +0530

    cod

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -10,12 +10,5 @@ const { isAuthenticatedUser } = require("../middleware/auth");
 router.route("/payment/process").post(isAuthenticatedUser, processPayment);
 
 router.route("/stripeapikey").get(isAuthenticatedUser, sendStripeApiKey);
-router.route("/payment/cod").post(async (req, res) => {
-  try {
-    await processPaymentCod(req, res);
-  } catch (error) {
-    console.error('Error in /payment/cod route:', error);
-    res.status(500).json({ success: false, error: 'Internal Server Error.' });
-  }
-});
+router.route("/payment/cod").post(isAuthenticatedUser, processPaymentCod);
 module.exports = router;
