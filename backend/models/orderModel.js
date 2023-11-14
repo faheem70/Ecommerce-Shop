3900139414187b21a690380e0d700ce6808a8992
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-11-14 22:55:53 +0530

    cod

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -61,7 +61,7 @@ const orderSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
-    required: true,
+    //required: true,
   },
   paymentInfo: {
     id: {
