26676832345b4a9ba1550827fded4e525b94b708
faheem70 <99425291+faheem70@users.noreply.github.com>
2025-10-26 11:30:54 +0530

    Add files via upload
    add category dynamically

---
+++ backend/routes/categoryRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const {
+  createCategory,
+  getAllCategories,
+  updateCategory,
+  deleteCategory,
+} = require("../controllers/categoryController");
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+const router = express.Router();
+
+router.route("/categories").get(getAllCategories);
+router.route("/admin/category/new").post(isAuthenticatedUser, authorizeRoles("admin"), createCategory);
+router.route("/admin/category/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateCategory)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteCategory);
+
+module.exports = router;
